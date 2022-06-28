69ca6754c6877fb9a40a377878390a7678fa0bec
andie <zchangan@163.com>
2022-06-28 20:30:02 +0800

    fix download error handling

--- bili_archiver/downloader.py
+++ bili_archiver/downloader.py
@@ -14,7 +14,7 @@ import tempfile
 
 ua = UserAgent().chrome
 slugify = partial(slugify.slugify, allow_unicode=True)
-tempdir = Path(tempfile.gettempdir())
+tempdir = Path(tempfile.gettempdir()).joinpath('bili_archiver')
 
 
 class DownloadException(Exception):
@@ -33,14 +33,13 @@ def download(api: BiliAPI, output: Path):
         video = parser.parse_video(api, aid)
         if video is None:
             continue
+        temp_parent = tempdir.joinpath(f"{video.up_id}_{slugify(video.up_name)}_{video.aid}_{slugify(video.title)}")
+        temp_parent.mkdir(exist_ok=True, parents=True)
+        parent = output.joinpath(f"{video.up_id}_{slugify(video.up_name)}/{video.aid}_{slugify(video.title)}")
+        parent.mkdir(exist_ok=True, parents=True)
         try:
-
-            temp_parent = tempdir.joinpath(f"{video.up_id}_{slugify(video.up_name)}_{video.aid}_{slugify(video.title)}")
-            temp_parent.mkdir(exist_ok=True, parents=True)
             referer = f'https://www.bilibili.com/video/{video.bvid}'
             for page in video.pages:
-                parent = output.joinpath(f"{video.up_id}_{slugify(video.up_name)}/{video.aid}_{slugify(video.title)}")
-                parent.mkdir(exist_ok=True, parents=True)
                 out_path = parent.joinpath(f'{page.pid}_{slugify(page.title)}.mp4')
                 if out_path.exists():
                     continue
@@ -60,14 +59,15 @@ def download(api: BiliAPI, output: Path):
                 logger.info(f'moving {temp_out_path} to {out_path}')
                 shutil.move(temp_out_path, out_path)
 
-            logger.info(f'removing {temp_parent}')
-            shutil.rmtree(temp_parent)
             recorder.download_history_set(video.aid, downloaded=True)
 
         except Exception as e:
             traceback.print_exc()
             logger.error(f'download failed, av: {video.aid} bv: {video.bvid}, title: {video.title}, reason: {e}')
             continue
+        finally:
+            logger.info(f'removing {temp_parent}')
+            shutil.rmtree(temp_parent)
 
 
 def merge(video_path: Path, audio_path: Path, out_path: Path):
